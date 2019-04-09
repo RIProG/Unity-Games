13b0304487a460f63e33631746236adb29d1a6af
RIProG <riprog@hotmail.com>
2019-04-09 20:08:10 +0200

    Finished

--- Unity2D/The Breakup Game/The Breakup Game/Assets/Scripts/Ball.cs
+++ Unity2D/The Breakup Game/The Breakup Game/Assets/Scripts/Ball.cs
@@ -1,30 +1,35 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // config parameters
+    // Config parameters
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = 0.2f;
 
-    //state
+    // State
     Vector2 paddleToBallVector;
     bool hasStarted = false;
+
+    // Cached component references
+    AudioSource myAudioSource;
+    Rigidbody2D myRigidBody2D;
 
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
+        myRigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         if (!hasStarted)
         {
-        LockBallToPaddle();
-        LaunchOnMouseClick();
+            LockBallToPaddle();
+            LaunchOnMouseClick();
         }
     }
 
@@ -33,14 +38,28 @@ public class Ball : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             hasStarted = true;
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            myRigidBody2D.velocity = new Vector2(xPush, yPush);
         }
-        
+
     }
 
     private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocityTweak = new Vector2
+            (Random.Range(0f, randomFactor),
+            Random.Range(0f, randomFactor));
+
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+            myRigidBody2D.velocity += velocityTweak;
+        }
     }
 }
